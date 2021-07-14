51a53d342a607edcc72b11abff69ae99c60f9ab5
Francis Uloko <francisuloko@yahoo.com>
2021-07-14 14:45:05 +0100

    Add task array and iterator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import _ from 'lodash'
 import './style.css'
 
 function component() {
-    const Tasks = [
-      {
-        description: 'Task descriptions',
-        completed: false,
-        index: 0,
-      }
-    ];
+  const toDoList = document.getElementById('to-do-list');
+  const tasks = [
+    {
+      description: 'Wake up',
+      completed: false,
+      index: 0
+    },
+    {
+      description: 'Eat breakfast',
+      completed: false,
+      index: 1
+    },
+    {
+      description: 'Write some code',
+      completed: false,
+      index: 2
+    }
+  ];
+
+  function createHTML(task){
+    let markup = `
+     <article>
+       <input type='checkbox' name='completed' id='${task.index}'>
+       <span>${task.description}</span>
+     </article>
+     `
+    return markup;
   }
+
+  tasks.forEach(item => {
+    toDoList.innerHTML += createHTML(item)
+  });
   
-document.body.appendChild(component());
+  return toDoList;
+}
+
+document.getElementById('to-do-list').appendChild(component());
