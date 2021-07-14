626bc7de52682a3b4352f3c4d83f046f7c09383e
Francis Uloko <francisuloko@yahoo.com>
2021-07-14 12:51:39 +0100

    Setup webpack and dev server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+import './style.css'
+
+function component() {
+    const Tasks = [
+      {
+        description: 'Task descriptions',
+        completed: false,
+        index: 0,
+      }
+    ];
+  }
+  
+document.body.appendChild(component());
