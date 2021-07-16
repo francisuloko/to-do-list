7b09061a512b2896bb0cce4ab1533e9fcf2a71b3
Francis Uloko <francisuloko@yahoo.com>
2021-07-16 09:48:53 +0100

    Fix linter errors

--- src/drag-and-drop.js
+++ src/drag-and-drop.js
@@ -6,28 +6,21 @@ function allowDrop(ev) {
 
 function drag(ev) {
   dragged = this;
-  ev.dataTransfer.setData("text", this.outerHTML);
+  ev.dataTransfer.setData('text', this.outerHTML);
 }
 
 function drop(ev) {
-  if(dragged != this) {
-    this.parentNode.removeChild(dragged)
-    const task = ev.dataTransfer.getData("text");
-    this.insertAdjacentHTML('beforebegin', task)
-    let dropped = this.previousSibling;
+  if (dragged !== this) {
+    this.parentNode.removeChild(dragged);
+    const task = ev.dataTransfer.getData('text');
+    this.insertAdjacentHTML('beforebegin', task);
+    const dropped = this.previousSibling;
     addEventsDragAndDrop(dropped);
   }
 }
 
-function addEventsDragAndDrop(elem) {
+export default function addEventsDragAndDrop(elem) {
   elem.addEventListener('dragstart', drag, false);
   elem.addEventListener('dragover', allowDrop, false);
   elem.addEventListener('drop', drop, false);
 }
-
-let tasks = document.querySelectorAll('.task-item');
-tasks.forEach(task => {
-  addEventsDragAndDrop(task);
-})
-
-export { addEventsDragAndDrop };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-import ToDoList from './to-do-list.js';
-import { addEventsDragAndDrop } from './drag-and-drop.js';
-import Task from './task.js'
+import ToDo from './todo.js';
+import addEventsDragAndDrop from './drag-and-drop.js';
+import Task from './task.js';
 import './style.css';
 
-
-const toDo = new ToDoList();
-const toDoList = document.getElementById('to-do-list');
-const tasks = document.querySelectorAll('task-item');
-
+const toDo = new ToDo();
 
 // Add new task
 document.getElementById('task-description').addEventListener('keypress', (event) => {
-  if(event.key === 'Enter'){
+  if (event.key === 'Enter') {
     event.preventDefault();
-    let description = document.getElementById("task-description");
-    let index = toDo.localStorageTasks.length;
-    let task = new Task(description.value, index);
+    const description = document.getElementById('task-description');
+    const index = toDo.localStorageTasks.length;
+    const task = new Task(description.value, index);
     description.value = '';
     toDo.add(task);
   }
 });
 
-let listItens = document.querySelectorAll('.task-item');
-[].forEach.call(listItens, function(item) {
-  addEventsDragAndDrop(item);
-});
+const tasks = document.querySelectorAll('#to-do-list .task-item');
+[].forEach.call(tasks, addEventsDragAndDrop);
