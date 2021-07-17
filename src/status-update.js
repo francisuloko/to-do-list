18d3fe14fd05dff962b9c502c01516237afeb1fa
Francis Uloko <francisuloko@yahoo.com>
2021-07-17 16:48:50 +0100

    Fix complete task functionality

--- src/status-update.js
+++ src/status-update.js
@@ -1,19 +1,50 @@
-import ToDo from './todo.js';
+export let list = [
+  {
+    description: 'This is the first test',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'This is the second test',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'This is the third test',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'This is the fourth test',
+    completed: false,
+    index: 3,
+  },
+];
 
-const todo = new ToDo();
+if (localStorage.getItem('list')) {
+   const localStorageTasks = JSON.parse(localStorage.getItem('list'));
+   list = localStorageTasks
+}
+
+export const save = () => {
+  localStorage.setItem('list', JSON.stringify(list));
+};
+
+
 const checkboxes = document.getElementsByClassName('checkbox');
-
 export default function checkboxesEvent(list) {
-  for (let i = 0; i < checkboxes.length; i += 1) {
-    checkboxes[i].addEventListener('change', () => {
-      if (list[i].completed === true) {
-        list[i].completed = false;
-        document.getElementById(`${todo.tasks[i].index}`).classList.remove('completed');
-      } else {
-        list[i].completed = true;
-        document.getElementById(`${todo.tasks[i].index}`).classList.add('completed');
-      }
-      todo.save();
-    });
-  }
+
+   for (let i = 0; i < checkboxes.length; i += 1) {
+     checkboxes[i].addEventListener('change', () => {
+       console.log(list[i].index);
+       if (list[i].completed === true) {
+         list[i].completed = false;
+         document.getElementById(`desc-${list[i].index}`).classList.remove('completed');
+       } else {
+         list[i].completed = true;
+         document.getElementById(`desc-${list[i].index}`).classList.add('completed');
+       }
+       save();
+     });
+   }
 }
